26a4180bf72aee22a1f102e063a0468b294ad81b
Danny <live626@gmail.com>
2022-01-20 20:14:01 -0400

    update: check if name already exist when adding a new person check if the name or the number is missing before adding a person

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -59,10 +59,16 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
 	const body = request.body;
 	
-	if(!body.content) {
+	if(!body.content || !body.name || !body.number) {
 		return response.status(400).json({
 			error: 'content missing'
 		})
+	}
+
+	const nameRegistered = persons.find(person => person.name === body.name);
+
+	if( nameRegistered ) {
+		return response.status(409).send({ error: 'name must be unique' })
 	}
 
 	const person = {
